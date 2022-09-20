9bd77420a659325be38923985b26295a8216e84e
mason white <masondwhite1@gmail.com>
2022-09-20 15:02:03 -0600

    commit tree e91a43c45a8915338375ffbf2af84d5b8e166c8f

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(tripDistance) {
+  if (tripDistance <= 200) {
+    return "This one is on me!"
+  } else if (tripDistance > 400 && tripDistance < 2000) {
+      return "That will be twenty bucks."
+  } else if (tripDistance > 2000 && tripDistance < 2500){
+      return "I will gladly take your thirty bucks."
+  } else if (tripDistance > 2500){
+      return "No can do."
+  }
+}
+/*
+function ternaryCheckCity(city) {
+  if (city === "NYC"){
+    return "Ok, sounds good."
+  } else{
+    return "No go."
+  }
+}
+*/
+
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go." 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip) {
+  let result
+  switch(tip){
+    case "generous": return "Thank you so much."
+    case "not as generous": return "Thank you."
+    default: return  "Bye."
+  } 
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
